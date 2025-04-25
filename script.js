2e6ceafb83b8b06c359376a1df92b479207f51a7
Andreas Kissner <andikiss500@gmail.com>
2025-04-25 07:08:15 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,452 @@
+let catalog = {
+  "info": {
+    "inventors": [
+      {
+        "image": "assets/img/jerry.png",
+        "name": "Jerry Siegel",
+        "role": "author",
+        "born": "1914-10-17",
+        "died": "1996-01-28"
+      },
+      {
+        "image": "./assets/img/joe.png",
+        "name": "Joe Shuster",
+        "role": "artist",
+        "born": "1914-07-10",
+        "died": "1992-07-30"
+      }
+    ],
+    "1930s": [
+      {
+        "image": "./assets/img/super1930.png",
+        "title": "Action Comics #1",
+        "year": 1938,
+        "description": "Erster Auftritt von Superman",
+        "creators": ["Jerry Siegel", "Joe Shuster"],
+        "color": "color",
+        "likes": 1250,
+        "liked": true,
+        "comments": [
+          [
+            {
+              "name": "ComicFan01",
+              "comment": "Einer der besten Superman-Stories aller Zeiten – absolut episch!"
+            },
+            {
+              "name": "ClassicCritic",
+              "comment": "Ein bisschen altmodisch erzählt, aber historisch sehr wichtig."
+            },
+            {
+              "name": "PowerReader",
+              "comment": "Interessante Wendung mit Supermans Kräften – habe ich so nicht erwartet!"
+            },
+            {
+              "name": "ArchivKenner",
+              "comment": "Ein echter Klassiker, gut gealtert. Ideal für Sammler."
+            },
+            {
+              "name": "JustLooking",
+              "comment": "Ich muss noch weiterlesen, aber das Cover hat mich sofort angesprochen."
+            }
+          ]
+
+        ]
+      },
+      {
+        "image": "./assets/img/super1930.png",
+        "title": "Superman #1",
+        "year": 1939,
+        "description": "Erstes Solo-Heft",
+        "creators": ["Jerry Siegel", "Joe Shuster"],
+        "color": "color",
+        "likes": 1250,
+        "liked": true,
+        "comments": [
+          [
+            {
+              "name": "ComicFan01",
+              "comment": "Einer der besten Superman-Stories aller Zeiten – absolut episch!"
+            },
+            {
+              "name": "ClassicCritic",
+              "comment": "Ein bisschen altmodisch erzählt, aber historisch sehr wichtig."
+            },
+            {
+              "name": "PowerReader",
+              "comment": "Interessante Wendung mit Supermans Kräften – habe ich so nicht erwartet!"
+            },
+            {
+              "name": "ArchivKenner",
+              "comment": "Ein echter Klassiker, gut gealtert. Ideal für Sammler."
+            },
+            {
+              "name": "JustLooking",
+              "comment": "Ich muss noch weiterlesen, aber das Cover hat mich sofort angesprochen."
+            }
+          ]
+
+        ]
+      },
+      {
+        "image": "./assets/img/super1930.png",
+        "title": "Newspaper Strip",
+        "year": 1939,
+        "description": "Tägliche Comicstrips in Zeitungen",
+        "creators": ["Jerry Siegel", "Joe Shuster"],
+        "color": "black-white", "likes": 1250,
+        "liked": true,
+        "comments": [
+          [
+            {
+              "name": "ComicFan01",
+              "comment": "Einer der besten Superman-Stories aller Zeiten – absolut episch!"
+            },
+            {
+              "name": "ClassicCritic",
+              "comment": "Ein bisschen altmodisch erzählt, aber historisch sehr wichtig."
+            },
+            {
+              "name": "PowerReader",
+              "comment": "Interessante Wendung mit Supermans Kräften – habe ich so nicht erwartet!"
+            },
+            {
+              "name": "ArchivKenner",
+              "comment": "Ein echter Klassiker, gut gealtert. Ideal für Sammler."
+            },
+            {
+              "name": "JustLooking",
+              "comment": "Ich muss noch weiterlesen, aber das Cover hat mich sofort angesprochen."
+            }
+          ]
+
+        ]
+      }
+    ],
+    "1940s": [
+      {
+        "image": "./assets/img/super1940.png",
+        "title": "Action Comics #1",
+        "year": 1938,
+        "description": "Erster Auftritt von Superman",
+        "creators": ["Jerry Siegel", "Joe Shuster"],
+        "color": "color",
+        "likes": 1250,
+        "liked": true,
+        "comments": [
+          [
+            {
+              "name": "ComicFan01",
+              "comment": "Einer der besten Superman-Stories aller Zeiten – absolut episch!"
+            },
+            {
+              "name": "ClassicCritic",
+              "comment": "Ein bisschen altmodisch erzählt, aber historisch sehr wichtig."
+            },
+            {
+              "name": "PowerReader",
+              "comment": "Interessante Wendung mit Supermans Kräften – habe ich so nicht erwartet!"
+            },
+            {
+              "name": "ArchivKenner",
+              "comment": "Ein echter Klassiker, gut gealtert. Ideal für Sammler."
+            },
+            {
+              "name": "JustLooking",
+              "comment": "Ich muss noch weiterlesen, aber das Cover hat mich sofort angesprochen."
+            }
+          ]
+
+        ]
+      },
+      {
+        "image": "./assets/img/super1940.png",
+        "title": "Superman #1",
+        "year": 1939,
+        "description": "Erstes Solo-Heft",
+        "creators": ["Jerry Siegel", "Joe Shuster"],
+        "color": "color",
+        "likes": 1250,
+        "liked": true,
+        "comments": [
+          [
+            {
+              "name": "ComicFan01",
+              "comment": "Einer der besten Superman-Stories aller Zeiten – absolut episch!"
+            },
+            {
+              "name": "ClassicCritic",
+              "comment": "Ein bisschen altmodisch erzählt, aber historisch sehr wichtig."
+            },
+            {
+              "name": "PowerReader",
+              "comment": "Interessante Wendung mit Supermans Kräften – habe ich so nicht erwartet!"
+            },
+            {
+              "name": "ArchivKenner",
+              "comment": "Ein echter Klassiker, gut gealtert. Ideal für Sammler."
+            },
+            {
+              "name": "JustLooking",
+              "comment": "Ich muss noch weiterlesen, aber das Cover hat mich sofort angesprochen."
+            }
+          ]
+
+        ]
+      },
+      {
+        "image": "./assets/img/super1940.png",
+        "title": "Newspaper Strip",
+        "year": 1939,
+        "description": "Tägliche Comicstrips in Zeitungen",
+        "creators": ["Jerry Siegel", "Joe Shuster"],
+        "color": "black-white",
+        "likes": 1250,
+        "liked": true,
+        "comments": [
+          [
+            {
+              "name": "ComicFan01",
+              "comment": "Einer der besten Superman-Stories aller Zeiten – absolut episch!"
+            },
+            {
+              "name": "ClassicCritic",
+              "comment": "Ein bisschen altmodisch erzählt, aber historisch sehr wichtig."
+            },
+            {
+              "name": "PowerReader",
+              "comment": "Interessante Wendung mit Supermans Kräften – habe ich so nicht erwartet!"
+            },
+            {
+              "name": "ArchivKenner",
+              "comment": "Ein echter Klassiker, gut gealtert. Ideal für Sammler."
+            },
+            {
+              "name": "JustLooking",
+              "comment": "Ich muss noch weiterlesen, aber das Cover hat mich sofort angesprochen."
+            }
+          ]
+
+        ]
+      }
+    ],
+    "1950s": [
+      {
+        "image": "./assets/img/super1950.png",
+        "title": "Superman's Pal Jimmy Olsen #1",
+        "year": 1954,
+        "description": "Start der Solo-Serie von Jimmy Olsen, Supermans Freund, mit kuriosen Abenteuern.",
+        "creators": ["Otto Binder", "Curt Swan"],
+        "color": "color",
+        "likes": 1120,
+        "liked": true,
+        "comments": [
+          [
+            {
+              "name": "ComicFan01",
+              "comment": "Einer der besten Superman-Stories aller Zeiten – absolut episch!"
+            },
+            {
+              "name": "ClassicCritic",
+              "comment": "Ein bisschen altmodisch erzählt, aber historisch sehr wichtig."
+            },
+            {
+              "name": "PowerReader",
+              "comment": "Interessante Wendung mit Supermans Kräften – habe ich so nicht erwartet!"
+            },
+            {
+              "name": "ArchivKenner",
+              "comment": "Ein echter Klassiker, gut gealtert. Ideal für Sammler."
+            },
+            {
+              "name": "JustLooking",
+              "comment": "Ich muss noch weiterlesen, aber das Cover hat mich sofort angesprochen."
+            }
+          ]
+
+        ]
+      },
+      {
+        "image": "./assets/img/super1950.png",
+        "title": "The Supergirl from Krypton!",
+        "year": 1959,
+        "description": "Erste Erscheinung von Supergirl (Kara Zor-El), Supermans Cousine.",
+        "creators": ["Otto Binder", "Al Plastino"],
+        "color": "color",
+        "likes": 1430,
+        "liked": true,
+        "comments": [
+          [
+            {
+              "name": "ComicFan01",
+              "comment": "Einer der besten Superman-Stories aller Zeiten – absolut episch!"
+            },
+            {
+              "name": "ClassicCritic",
+              "comment": "Ein bisschen altmodisch erzählt, aber historisch sehr wichtig."
+            },
+            {
+              "name": "PowerReader",
+              "comment": "Interessante Wendung mit Supermans Kräften – habe ich so nicht erwartet!"
+            },
+            {
+              "name": "ArchivKenner",
+              "comment": "Ein echter Klassiker, gut gealtert. Ideal für Sammler."
+            },
+            {
+              "name": "JustLooking",
+              "comment": "Ich muss noch weiterlesen, aber das Cover hat mich sofort angesprochen."
+            }
+          ]
+
+        ]
+      },
+      {
+        "image": "./assets/img/super1950.png",
+        "title": "The Battle with Bizarro!",
+        "year": 1959,
+        "description": "Debüt von Bizarro, einem fehlgeschlagenen Superman-Klon mit verzerrter Logik.",
+        "creators": ["Otto Binder", "George Papp"],
+        "color": "color",
+        "likes": 1260,
+        "liked": false,
+        "comments": [
+          [
+            {
+              "name": "ComicFan01",
+              "comment": "Einer der besten Superman-Stories aller Zeiten – absolut episch!"
+            },
+            {
+              "name": "ClassicCritic",
+              "comment": "Ein bisschen altmodisch erzählt, aber historisch sehr wichtig."
+            },
+            {
+              "name": "PowerReader",
+              "comment": "Interessante Wendung mit Supermans Kräften – habe ich so nicht erwartet!"
+            },
+            {
+              "name": "ArchivKenner",
+              "comment": "Ein echter Klassiker, gut gealtert. Ideal für Sammler."
+            },
+            {
+              "name": "JustLooking",
+              "comment": "Ich muss noch weiterlesen, aber das Cover hat mich sofort angesprochen."
+            }
+          ]
+
+        ]
+      }
+    ],
+    "1960s": [
+      {
+        "image": "./assets/img/super1960.png",
+        "title": "The Last Days of Superman!",
+        "year": 1962,
+        "description": "Superman glaubt, dass er sterben wird, und verabschiedet sich von seinen Freunden – ein emotionaler Klassiker.",
+        "creators": ["Edmond Hamilton", "Curt Swan"],
+        "color": "color",
+        "likes": 1600,
+        "liked": true,
+        "comments": [
+          [
+            {
+              "name": "ComicFan01",
+              "comment": "Einer der besten Superman-Stories aller Zeiten – absolut episch!"
+            },
+            {
+              "name": "ClassicCritic",
+              "comment": "Ein bisschen altmodisch erzählt, aber historisch sehr wichtig."
+            },
+            {
+              "name": "PowerReader",
+              "comment": "Interessante Wendung mit Supermans Kräften – habe ich so nicht erwartet!"
+            },
+            {
+              "name": "ArchivKenner",
+              "comment": "Ein echter Klassiker, gut gealtert. Ideal für Sammler."
+            },
+            {
+              "name": "JustLooking",
+              "comment": "Ich muss noch weiterlesen, aber das Cover hat mich sofort angesprochen."
+            }
+          ]
+
+        ]
+      },
+      {
+        "image": "./assets/img/super1960.png",
+        "title": "Superman's New Power!",
+        "year": 1960,
+        "description": "Superman entdeckt eine neue Energie-Projektil-Kraft, verliert aber vorübergehend seine anderen Fähigkeiten.",
+        "creators": ["Jerry Siegel", "Wayne Boring"],
+        "color": "color",
+        "likes": 1150,
+        "liked": false,
+        "comments": [
+          [
+            {
+              "name": "ComicFan01",
+              "comment": "Einer der besten Superman-Stories aller Zeiten – absolut episch!"
+            },
+            {
+              "name": "ClassicCritic",
+              "comment": "Ein bisschen altmodisch erzählt, aber historisch sehr wichtig."
+            },
+            {
+              "name": "PowerReader",
+              "comment": "Interessante Wendung mit Supermans Kräften – habe ich so nicht erwartet!"
+            },
+            {
+              "name": "ArchivKenner",
+              "comment": "Ein echter Klassiker, gut gealtert. Ideal für Sammler."
+            },
+            {
+              "name": "JustLooking",
+              "comment": "Ich muss noch weiterlesen, aber das Cover hat mich sofort angesprochen."
+            }
+          ]
+
+        ]
+      },
+      {
+        "image": "./assets/img/super1960.png",
+        "title": "The Super-Duel in Space",
+        "year": 1961,
+        "description": "Superman kämpft gegen Brainiac in einem intergalaktischen Showdown – ein Sci-Fi-Highlight der Ära.",
+        "creators": ["Otto Binder", "Curt Swan"],
+        "color": "color",
+        "likes": 1375,
+        "liked": true,
+        "comments": [
+          [
+            {
+              "name": "ComicFan01",
+              "comment": "Einer der besten Superman-Stories aller Zeiten – absolut episch!"
+            },
+            {
+              "name": "ClassicCritic",
+              "comment": "Ein bisschen altmodisch erzählt, aber historisch sehr wichtig."
+            },
+            {
+              "name": "PowerReader",
+              "comment": "Interessante Wendung mit Supermans Kräften – habe ich so nicht erwartet!"
+            },
+            {
+              "name": "ArchivKenner",
+              "comment": "Ein echter Klassiker, gut gealtert. Ideal für Sammler."
+            },
+            {
+              "name": "JustLooking",
+              "comment": "Ich muss noch weiterlesen, aber das Cover hat mich sofort angesprochen."
+            }
+          ]
+
+        ]
+      }
+    ],
+  }
+}
+
+
+console.log(catalog);
+let img = document.getElementById("output");
+img.src = catalog.info.inventors[0].image; // Setzt den src-Wert des img-Elements
